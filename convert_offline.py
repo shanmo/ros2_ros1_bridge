7b25e34a0da492b4aab5e2d6641bc82c16b1b822
uie85879 <mo.shan@continental-corporation.com>
2022-06-08 12:46:01 +0800

    save ros2 bag

--- convert_offline.py
+++ convert_offline.py
@@ -3,56 +3,73 @@ from rosbags.serde import deserialize_cdr, ros1_to_cdr
 
 from rosbags.rosbag2 import Writer
 from rosbags.serde import serialize_cdr
-from rosbags.typesys.types import std_msgs__msg__String as String
-from rosbags.typesys.types import builtin_interfaces__msg__Time as Time
 from rosbags.typesys.types import sensor_msgs__msg__CompressedImage as CompressedImage
-from rosbags.typesys.types import std_msgs__msg__Header as Header
-
-image_msgs = []
-lidar_msgs = []
-tf_msgs = []
-timestamps = []
-
-ros1_bag_name = './data/ros1_demo.bag'
-ros2_bag_name = './data/ros2_demo'
-
-# create reader instance
-with Reader(ros1_bag_name) as reader:
-    # iterate over messages
-    for connection, timestamp, rawdata in reader.messages():
-        timestamps.append(timestamp)
-
-        if connection.topic == '/fr_camera/color/image_raw_throttled/compressed':
-            msg = deserialize_cdr(ros1_to_cdr(rawdata, connection.msgtype), connection.msgtype)
-            image_msgs.append(msg)
-            print(f"compressed image {msg.header.frame_id}")
-
-        if connection.topic == '/rslidar_points':
-            msg = deserialize_cdr(ros1_to_cdr(rawdata, connection.msgtype), connection.msgtype)
-            lidar_msgs.append(msg)
-            print(f"lidar {msg.header.frame_id}")
-
-        if connection.topic == '/tf_static':
-            msg = deserialize_cdr(ros1_to_cdr(rawdata, connection.msgtype), connection.msgtype)
-            tf_msgs.append(msg)
+from rosbags.typesys.types import sensor_msgs__msg__PointCloud2 as pt2
+from rosbags.typesys.types import tf2_msgs__msg__TFMessage as tf_m
 
 from pathlib import Path
 import shutil
 
-dirpath = Path(ros2_bag_name)
-if dirpath.exists() and dirpath.is_dir():
-    shutil.rmtree(dirpath)
+def read_topic(ros1_bag_name, topic_name):
+    results = []
+    # create reader instance
+    with Reader(ros1_bag_name) as reader:
+        # iterate over messages
+        for connection, timestamp, rawdata in reader.messages():
+            if connection.topic == topic_name:
+                msg = deserialize_cdr(ros1_to_cdr(rawdata, connection.msgtype), connection.msgtype)
+                results.append((timestamp, msg))
+                # print(f"read {msg.header.frame_id}")
+    return results
 
-# create writer instance and open for writing
-with Writer(ros2_bag_name) as writer:
-    # add new connection
-    topic = '/image_compressed'
-    msgtype = CompressedImage.__msgtype__
-    connection = writer.add_connection(topic, msgtype, 'cdr', '')
+if __name__ == "__main__":
+    ros1_bag_name = './data/ros1_demo.bag'
+    ros2_bag_name = './data/ros2_demo'
 
-    for timestamp, img in zip(timestamps, image_msgs):
-        writer.write(
-            connection,
-            timestamp,
-            serialize_cdr(img, msgtype),
-        )
+    dirpath = Path(ros2_bag_name)
+    if dirpath.exists() and dirpath.is_dir():
+        shutil.rmtree(dirpath)
+
+    topic_name = "/fr_camera/color/image_raw_throttled/compressed"
+    img_msgs = read_topic(ros1_bag_name, topic_name)
+
+    topic_name = "/rslidar_points"
+    lidar_msgs = read_topic(ros1_bag_name, topic_name)
+
+    topic_name = "/tf_static"
+    tf_msgs = read_topic(ros1_bag_name, topic_name)
+
+    # create writer instance and open for writing
+    with Writer(ros2_bag_name) as writer:
+        # add new connection
+        topic_name = "/image_compressed"
+        img_msgtype = CompressedImage.__msgtype__
+        img_conn = writer.add_connection(topic_name, img_msgtype, 'cdr', '')
+        for timestamp, msg in img_msgs:
+            writer.write(
+                img_conn,
+                timestamp,
+                serialize_cdr(msg, img_msgtype),
+            )
+
+        # add new connection
+        topic_name = "/lidar"
+        ld_msgtype = pt2.__msgtype__
+        ld_conn = writer.add_connection(topic_name, ld_msgtype, 'cdr', '')
+        for timestamp, msg in lidar_msgs:
+            writer.write(
+                ld_conn,
+                timestamp,
+                serialize_cdr(msg, ld_msgtype),
+            )
+
+        # add new connection
+        topic_name = "/tf"
+        tf_msgtype = tf_m.__msgtype__
+        tf_conn = writer.add_connection(topic_name, tf_msgtype, 'cdr', '')
+        for timestamp, msg in tf_msgs:
+            writer.write(
+                tf_conn,
+                timestamp,
+                serialize_cdr(msg, tf_msgtype),
+            )
