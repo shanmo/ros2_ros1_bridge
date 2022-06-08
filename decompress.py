9447ccf2d097c2843699c9fa5c676d2792b049ea
uie85879 <mo.shan@continental-corporation.com>
2022-06-08 15:11:09 +0800

    output correct

--- decompress.py
+++ decompress.py
@@ -1,4 +1,4 @@
-from cv_bridge import CvBridge, CvBridgeError
+from cv_bridge import CvBridge
 import numpy as np
 import cv2
 
@@ -14,6 +14,11 @@ class ImageTools(object):
     def convert_ros_compressed_msg_to_ros_msg(self, compressed_msg,
                                               encoding='rgb8'):
         cv2_img = self.convert_ros_compressed_to_cv2(compressed_msg)
+        # print(f"cv2_img.shape {cv2_img.shape}")
+        # cv2.imshow("image", cv2_img)
+        # key = cv2.waitKey(0)
+        # if chr(key) == 'q':
+        #     cv2.destroyWindow("image")
         ros_img = self._cv_bridge.cv2_to_imgmsg(cv2_img, encoding=encoding)
         return ros_img
 
